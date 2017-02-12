4f5e9f6eca30810a44f18f7090da5ac9f36039fc
Nathan Stitt <nathan@stitt.org>
2017-02-12 17:52:32 -0600

    neaten up imports & exports

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-import model from './lib/class-decorator';
-import { lookupModelUsing, rememberModelUsing } from './lib/model-lookup';
-import { field, session, belongsTo, hasMany, identifier } from './lib/property-decorators';
-
-export {
-    model,
-    identifier,
-    field,
-    session,
-    belongsTo,
-    hasMany,
-    lookupModelUsing,
-    rememberModelUsing,
-};
+export * from './lib/model-lookup';
+export * from './lib/class-decorator';
+export * from './lib/property-decorators';
