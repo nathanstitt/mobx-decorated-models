7d9c104991a0b56ed16a27772cde84c37331da91
Nathan Stitt <nathan@stitt.org>
2018-04-15 21:52:34 -0500

    initialize when get as well as set is called

--- specs/property-decorators.spec.js
+++ specs/property-decorators.spec.js
@@ -188,17 +188,20 @@ describe('Property Decorators', () => {
     });
 
     it('retains hasMany collection instead of replacing it', () => {
-        const container = Container.deserialize({ id: 1, color: 'red' });
+        const container = new Container({ id: 1, color: 'red' });
         expect(container.boxes).toHaveLength(0);
-        container.boxes.foo = 1; // Container.deserialize({ id: 1, color: 'red' });
-        container.update({ boxes: [{ width: 12, height: 12, depth: 12 }] });
+        const originalBoxes = container.boxes;
+        container.boxes.foo = 1;
+        container.boxes = [{ width: 1 }];
+        expect(container.boxes).toBe(originalBoxes);
         expect(container.boxes.foo).toEqual(1);
-
+        container.update({ boxes: [{ width: 12, height: 12, depth: 12 }] });
         expect(container.boxes).toHaveLength(1);
         expect(container.boxes[0]).toBeInstanceOf(Box);
         expect(container.boxes[0].color).toEqual('red');
         expect(container.boxes[0]).toBeInstanceOf(Box);
         container.boxes = [];
+        expect(container.boxes).toBe(originalBoxes);
         expect(container.boxes.foo).toEqual(1);
         container.boxes.push({ width: 10, height: 10, depth: 10 });
         expect(container.boxes[0]).toBeInstanceOf(Box);
